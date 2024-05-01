aee1138f7bec749cbce626e9eb9b0547f241a53e
Ebenezer Frimpong <ebenezerfrimpong17@gmail.com>
2024-05-01 02:16:48 -0400

    Create navbar components:

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -23,31 +23,30 @@ export default function About() {
       <h1 className="font-2xl font-medium text-amber-600">
         What Sets Us Apart
       </h1>
-      <p>
-        <div>
-          <b>Comprehensive Data:</b> Our database is constantly updated with
-          information from various sources. While we actively gather details
-          from studios and filmmakers, the bulk of our content comes from
-          industry insiders and passionate fans like you.
-        </div>
-        <div>
-          <b>Ratings and Reviews:</b> Want to know if a movie is worth watching?
-          Check out our user-generated ratings and reviews. We believe in the
-          power of community opinions, and we encourage everyone to share their
-          thoughts.
-        </div>
-        <div>
-          <b>Personalized Recommendations:</b> Tired of scrolling through
-          endless options? Let us help! Our personalized recommendation engine
-          suggests the perfect show or film based on your preferences.
-        </div>
-        <div>
-          <b>Exclusive Content:</b> Dive into our IMDb Originals—exclusive
-          celebrity interviews, behind-the-scenes stories, and expert analysis.
-          From trending entertainment news to in-depth discussions, we’ve got it
-          all
-        </div>
-      </p>
+
+      <div>
+        <b>Comprehensive Data:</b> Our database is constantly updated with
+        information from various sources. While we actively gather details from
+        studios and filmmakers, the bulk of our content comes from industry
+        insiders and passionate fans like you.
+      </div>
+      <div>
+        <b>Ratings and Reviews:</b> Want to know if a movie is worth watching?
+        Check out our user-generated ratings and reviews. We believe in the
+        power of community opinions, and we encourage everyone to share their
+        thoughts.
+      </div>
+      <div>
+        <b>Personalized Recommendations:</b> Tired of scrolling through endless
+        options? Let us help! Our personalized recommendation engine suggests
+        the perfect show or film based on your preferences.
+      </div>
+      <div>
+        <b>Exclusive Content:</b> Dive into our IMDb Originals—exclusive
+        celebrity interviews, behind-the-scenes stories, and expert analysis.
+        From trending entertainment news to in-depth discussions, we’ve got it
+        all
+      </div>
     </div>
   );
 }

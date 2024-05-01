4b8743fab20f99cab802c8eb4cc43a5e1fac87f8
Ebenezer Frimpong <ebenezerfrimpong17@gmail.com>
2024-05-01 11:10:08 -0400

    Add Card Component

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center items-center mt-16'>
+      <img className='h-52' src='spinner.svg' alt='loading...' />
+    </div>
+  )
+}

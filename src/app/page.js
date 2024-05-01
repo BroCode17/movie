db08685f7fa27e74d6a8523b63ded0944f6971a1
Ebenezer Frimpong <ebenezerfrimpong17@gmail.com>
2024-05-01 02:47:57 -0400

    handling error

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import Results from './components/Results'
+const API_KEY = process.env.API_KEY
+const  HomePage = async ({searchParams}) => {
+  const genre  = searchParams.genre || 'fetchTrending'
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'}?api_key=${API_KEY}&
+      language=en-US&page=1
+    `
+  )
 
-const HomePage = () => {
+  const data = await res.json();
+
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+  const results = data.results
+
   return (
-    <div>
-      HomePage
-    </div>
+    <Results results={results} />
   )
 }
 
